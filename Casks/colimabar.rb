fb3dc1f6b6b19ae4d36069908c7dded4165304e5
dreuse <sueder.morais@recordedfuture.com>
2026-04-09 22:54:51 +0200

    Update colimabar to v0.3.2

--- Casks/colimabar.rb
+++ Casks/colimabar.rb
@@ -1,6 +1,6 @@
 cask "colimabar" do
-  version "0.3.1"
-  sha256 "46b86b7e787ac5cecd691d6e3d19f4eab0be7716a53a13b1c0157cae7fc31941"
+  version "0.3.2"
+  sha256 "55566899d06de0312ca1c5a7ee06812b21b4058988bd9c5f0461e2caf8c0a746"
 
   url "https://github.com/dreuse/ColimaBar/releases/download/v#{version}/ColimaBar.zip"
   name "ColimaBar"
